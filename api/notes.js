24de8bd2ce30166977b439fe918808b9844949c5
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-01-20 20:16:18 -0600

    Create notes.js

---
+++ api/notes.js
@@ -0,0 +1,22 @@
+const Note = require('./notesDB');
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const notes = await Note.find();
+      res.json(notes);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else if (req.method === 'POST') {
+    const { content, position } = req.body;
+
+    try {
+      const newNote = new Note({ content, position });
+      await newNote.save();
+      res.status(201).json(newNote);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
